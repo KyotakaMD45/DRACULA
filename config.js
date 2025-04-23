b60cd0a3f2d1869233c9d89afd2955b566bb3e44
KyotakaMD45 <daemonabraxas08@gmail.com>
2025-04-23 13:34:25 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "KHAN-MD~kT0zSCSB#Vw1qLNBOVa9EoaVi9cQTPWiXGqLvgpsvv57aEGNJekk",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
